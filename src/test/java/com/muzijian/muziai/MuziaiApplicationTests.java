aa70c72bb84ce2852eced0091cce79b833bd1620
lishangzhong <lishangzhong2007@126.com>
2019-02-26 14:05:16 +0800

    first commit

---
+++ src/test/java/com/muzijian/muziai/MuziaiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muzijian.muziai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuziaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
